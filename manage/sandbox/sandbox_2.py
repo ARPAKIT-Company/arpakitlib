c4a801d78e06fbce5d831210571e34aec75b7693
arpakit <arpakit@gmail.com>
2024-12-10 05:46:54 +0500

    no_message

--- manage/sandbox/sandbox_2.py
+++ manage/sandbox/sandbox_2.py
@@ -1,13 +1,8 @@
 import asyncio
 
-from arpakitlib.ar_http_request_util import sync_make_http_request
-from arpakitlib.ar_type_util import raise_for_type
-
 
 def command():
-    for a in range(100):
-        res = sync_make_http_request(url="http://127.0.0.1:8000/create_healthcheck")
-        print(res.status_code)
+    pass
 
 
 async def async_command():
