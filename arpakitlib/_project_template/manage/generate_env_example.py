8c09e3ffad83412b6467358b47863fc61b144c24
arpakit <arpakit@gmail.com>
2024-12-22 15:31:21 +0500

    no_message

--- arpakitlib/_project_template/manage/generate_env_example.py
+++ arpakitlib/_project_template/manage/generate_env_example.py
@@ -1,7 +1,7 @@
 import os.path
 
-from project_template.src.core.const import BASE_DIRPATH
-from project_template.src.core.settings import Settings
+from src.core.const import BASE_DIRPATH
+from src.core.settings import Settings
 
 
 def command():
--- project_template/manage/poetry_show_arpakitlib.sh
+++
@@ -1,2 +0,0 @@
-cd ..
-poetry show arpakitlib --tree
