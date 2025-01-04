9ff78c3ebed62bcf20563c3a1f3324790144571d
arpakit <arpakit@gmail.com>
2025-01-04 06:04:42 +0500

    fix

--- manage/git_set_arpakit_company_origin.sh
+++ manage/git_set_arpakit_company_origin.sh
@@ -1,7 +1,7 @@
 cd ..
 
 git remote remove arpakit_company_github_1
-git remote add arpakit_company_github_1 git@github.com:ARPAKIT-Company/arpakit.git
+git remote add arpakit_company_github_1 git@github.com:ARPAKIT-Company/arpakitlib.git
 
 git remote remove arpakit_company_gitlab_1
-git remote add arpakit_company_gitlab_1 git@gitlab.com:ARPAKIT-Company/arpakit.git
+git remote add arpakit_company_gitlab_1 git@gitlab.com:ARPAKIT-Company/arpakitlib.git
