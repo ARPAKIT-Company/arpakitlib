b72460a32093ea80e30d5d47c2fadc1b933b524c
arpakit <arpakit@gmail.com>
2025-01-12 19:03:43 +0500

    fix

--- manage/sandbox/sandbox_1.py
+++ manage/sandbox/sandbox_1.py
@@ -1,31 +1,23 @@
 import asyncio
+import logging
 
 from arpakitlib.ar_logging_util import setup_normal_logging
 from arpakitlib.ar_operation_execution_util import OperationExecutorWorker
 from arpakitlib.ar_sqlalchemy_util import SQLAlchemyDB
+from manage.sandbox.hgjk.g import go
 from src.operation_execution.operation_executor import OperationExecutor
 
 
 class A(OperationExecutorWorker):
-    async def async_on_startup(self):
-        raise Exception
+    pass
 
 
 def command():
-    pass
+    setup_normal_logging()
+    go()
 
 async def async_command():
-    setup_normal_logging(log_filepath="./story.log")
-    await A(
-        sqlalchemy_db=SQLAlchemyDB(
-            db_url="postgresql://arpakitlib:arpakitlib@127.0.0.1:50517/arpakitlib"
-        ),
-        operation_executor=OperationExecutor(
-            sqlalchemy_db=SQLAlchemyDB(
-                db_url="postgresql://arpakitlib:arpakitlib@127.0.0.1:50517/arpakitlib"
-            )
-        )
-    ).async_safe_run()
+    pass
 
 
 if __name__ == '__main__':
