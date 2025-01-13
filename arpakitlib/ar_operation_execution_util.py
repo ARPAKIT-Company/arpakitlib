07637e73ac27933fc11c55f9b8a6ff9a31adf79e
arpakit <arpakit@gmail.com>
2025-01-13 06:11:39 +0500

    fix

--- arpakitlib/ar_operation_execution_util.py
+++ arpakitlib/ar_operation_execution_util.py
@@ -441,24 +441,25 @@ class ScheduledOperationCreatorWorker(BaseWorker):
             await async_safe_sleep(n=timeout)
 
 
-def every_timedelta_is_time_func(*, td: timedelta) -> Callable:
+def every_timedelta_is_time_func(*, td: timedelta, now_dt_func: Callable = now_utc_dt) -> Callable:
     last_now_utc_dt = now_utc_dt()
 
     def func() -> bool:
         nonlocal last_now_utc_dt
-        now_utc_dt_ = now_utc_dt()
-        if (now_utc_dt_ - last_now_utc_dt) >= td:
-            last_now_utc_dt = now_utc_dt_
+        now_dt_func_ = now_dt_func()
+        if (now_dt_func_ - last_now_utc_dt) >= td:
+            last_now_utc_dt = now_dt_func_
             return True
         return False
 
     return func
 
 
-def between_different_times_is_time_func(*, from_time: time, to_time: time) -> Callable:
+def between_different_times_is_time_func(
+        *, from_time: time, to_time: time, now_dt_func: Callable = now_utc_dt
+) -> Callable:
     def func() -> bool:
-        now_utc_dt_ = now_utc_dt()
-        if from_time <= now_utc_dt_.time() <= to_time:
+        if from_time <= now_dt_func().time() <= to_time:
             return True
         return False
 
