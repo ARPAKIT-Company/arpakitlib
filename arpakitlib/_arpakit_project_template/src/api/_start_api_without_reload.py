000535a74bd6e4ebc3fcba0ceeccf78a2c8afa99
arpakit <arpakit@gmail.com>
2025-01-25 23:21:53 +0500

    fix

--- arpakitlib/_arpakit_project_template/src/api/_start_api_without_reload.py
+++ arpakitlib/_arpakit_project_template/src/api/_start_api_without_reload.py
@@ -1,9 +1,11 @@
 import uvicorn
 
 from src.core.settings import get_cached_settings
+from src.core.util import setup_logging
 
 
 def _start_api_for_dev_without_reload():
+    setup_logging()
     uvicorn.run(
         "src.api.asgi:app",
         port=get_cached_settings().api_port,
