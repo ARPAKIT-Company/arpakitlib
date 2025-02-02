aa338c9c4eb295e39bf3c70df3bcd4dc4111f8a8
njmin <gazinurnurtdinov35@gmail.com>
2025-02-02 23:12:30 +0500

    no comment

--- arpakitlib/ar_arpakit_schedule_uust_site_util.py
+++ arpakitlib/ar_arpakit_schedule_uust_site_util.py
@@ -7,55 +7,43 @@ from arpakitlib.ar_type_util import raise_for_type
 _ARPAKIT_LIB_MODULE_VERSION = "3.0"
 
 
-def generate_arpakit_schedule_uust_site_url(base_url: str = "https://schedule-uust.arpakit.com",
-                                            entity_type: str = None,
-                                            uust_api_id: int = None,
-                                            session: bool = None,
-                                            week: int = None,
-                                            theme: str = None) -> str:
-    if not base_url.endswith("/"):
-        base_url += "/"
-
+def generate_arpakit_schedule_uust_site_url(*,
+                                            base_url: str = "https://schedule-uust.arpakit.com",
+                                            entity_type: str | None = None,
+                                            uust_api_id: int | None = None,
+                                            session: bool | None = None,
+                                            week: int | None = None,
+                                            theme: str | None = None) -> str:
     params = {}
 
-    if entity_type is not None and entity_type in ["group", "teacher"]: # group/teacher
+    raise_for_type(base_url, str)
+
+    if entity_type is not None: # group/teacher
         raise_for_type(entity_type, str)
         params['entity_type'] = entity_type
-    else:
-        return base_url
 
     if uust_api_id is not None:
         raise_for_type(uust_api_id, int)
         params['uust_api_id'] = uust_api_id
-    else:
-        return base_url
 
     if session is not None: # false/true
         raise_for_type(session, bool)
-
         if session:
             params['session'] = "true"
         else:
             params['session'] = "false"
-    else:
-        params['session'] = "false"
 
     if week is not None:
         raise_for_type(week, int)
         params['week'] = week
-    else:
-        return base_url
 
-    if theme is not None and theme in ["dark", "light"]: # dark/light
+    if theme is not None: # dark/light
         raise_for_type(theme, str)
         params['theme'] = theme
-    else:
-        params['theme'] = "light"
 
 
     if params:
-        query_string  = urlencode(params)
-        result_url = urljoin(base_url, f"schedule?{query_string}")
+        result_url = urljoin(base_url, f"schedule?{urlencode(params)}")
     else:
         result_url = base_url
 
