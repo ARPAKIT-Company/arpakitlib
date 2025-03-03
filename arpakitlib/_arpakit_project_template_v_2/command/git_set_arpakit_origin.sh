c8b53cfed9904cebc2b8f7fa1a4b2276985e92d2
arpakit <arpakit@gmail.com>
2025-03-03 18:14:54 +0500

    fix

--- arpakitlib/_arpakit_project_template_v_2/command/git_set_arpakit_origin.sh
+++ arpakitlib/_arpakit_project_template_v_2/command/git_set_arpakit_origin.sh
@@ -7,3 +7,5 @@ git remote add arpakit_github_1 git@github.com:arpakit/${project_name}.git
 
 git remote remove arpakit_gitlab_1
 git remote add arpakit_gitlab_1 git@gitlab.com:arpakit/${project_name}.git
+
+git remote -v
--- arpakitlib/_arpakit_project_template_v_2/command/git_set_divarteam_origin.sh
+++ arpakitlib/_arpakit_project_template_v_2/command/git_set_divarteam_origin.sh
@@ -4,3 +4,5 @@ source .env
 
 git remote remove divarteam_github_1
 git remote add divarteam_github_1 git@github.com:divarteam/${project_name}.git
+
+git remote -v
