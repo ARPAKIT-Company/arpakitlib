41b97871b7388a9d9430383db07cd468dd7b3c93
arpakit <arpakit@gmail.com>
2025-03-12 07:00:26 +0500

    fix

--- arpakitlib/ar_blank_util.py
+++ arpakitlib/ar_blank_util.py
@@ -4,5 +4,5 @@ _ARPAKIT_LIB_MODULE_VERSION = "3.0"
 
 
 class BaseBlank:
-    def __init__(self, **kwargs):
-        pass
+    def __init__(self, lang: str = "ru", **kwargs):
+        self.lang = lang
