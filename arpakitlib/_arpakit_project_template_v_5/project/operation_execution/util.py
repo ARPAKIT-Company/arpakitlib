638fffbbbd4407cd9b9bd9e6681da4d146cd2c17
arpakit <arpakit@gmail.com>
2025-03-27 12:19:55 +0500

    fix

--- arpakitlib/_arpakit_project_template_v_5/project/operation_execution/util.py
+++ arpakitlib/_arpakit_project_template_v_5/project/operation_execution/util.py
@@ -4,13 +4,18 @@ from typing import Callable
 from arpakitlib.ar_datetime_util import now_utc_dt
 
 
-def every_timedelta_is_time_func(*, td: timedelta, now_dt_func: Callable = now_utc_dt) -> Callable:
-    last_now_utc_dt = now_utc_dt()
+def every_timedelta_is_time_func(
+        *,
+        td: timedelta,
+        now_dt_func: Callable = now_utc_dt,
+        create_first_time: bool = False
+) -> Callable:
+    last_now_utc_dt = None if create_first_time is not None else now_dt_func()
 
     def func() -> bool:
         nonlocal last_now_utc_dt
         now_dt_func_ = now_dt_func()
-        if (now_dt_func_ - last_now_utc_dt) >= td:
+        if last_now_utc_dt is None or (now_dt_func_ - last_now_utc_dt) >= td:
             last_now_utc_dt = now_dt_func_
             return True
         return False
