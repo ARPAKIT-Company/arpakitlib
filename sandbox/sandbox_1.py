70e69e380aa3d0aed87a322e089778121404859b
arpakit <arpakit@gmail.com>
2025-04-09 17:41:03 +0500

    fix

--- sandbox/sandbox_1.py
+++ sandbox/sandbox_1.py
@@ -2,8 +2,13 @@ import asyncio
 
 
 def __sandbox():
-    a = ["asfaf"]
-    print(" and ".join(a))
+    all_realization_term_ids = [1,2,3,4]
+    chunk_size = 3
+    chunked_list = [
+        all_realization_term_ids[i:i + chunk_size]
+        for i in range(0, len(all_realization_term_ids), chunk_size)
+    ]
+    print(chunked_list)
 
 
 async def __async_sandbox():
