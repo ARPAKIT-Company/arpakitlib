3bc4545503511494de7c3fd228d820ee39057a0c
arpakit <arpakit@gmail.com>
2025-06-15 17:27:04 +0500

    no_message

---
+++ command/update_on_every_remote_resources_without_pypi.sh
@@ -0,0 +1,12 @@
+cd ..
+
+git add .
+git commit -m "fix"
+git push arpakit_company_github_1
+git push arpakit_company_gitlab_1
+
+export TWINE_HTTP_TIMEOUT=60
+
+rm -rf dist
+poetry build
+poetry publish --repository testpypi
