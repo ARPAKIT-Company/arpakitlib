9deb2d6e948d0cd5eb6a110c8832cbc798f5c3c8
arpakit <arpakit@gmail.com>
2025-10-24 01:38:30 +0500

    fix

--- arpakitlib/ar_settings_util.py
+++ arpakitlib/ar_settings_util.py
@@ -13,7 +13,7 @@ def generate_env_example(settings_class: Union[BaseSettings, type[BaseSettings]]
     for k, f in settings_class.model_fields.items():
         if f.default is not PydanticUndefined:
             # есть дефолтное значение — пишем его как комментарий
-            res += f"# {k}={f.default}\n"
+            res += f"{k}={f.default}\n"
         else:
             # обязательное поле — без комментария
             res += f"{k}=\n"
