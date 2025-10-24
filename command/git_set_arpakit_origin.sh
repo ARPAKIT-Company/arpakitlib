0b179e7d6dbf7e10e1de5eb425e6bf9c4e99ba43
arpakit <arpakit@gmail.com>
2025-10-24 20:52:05 +0500

    fix

--- command/git_set_arpakit_origin.sh
+++
@@ -1,7 +0,0 @@
-cd ..
-
-git remote remove arpakit_github_1
-git remote add arpakit_github_1 git@github.com:arpakit/arpakitlib.git
-
-git remote remove arpakit_gitlab_1
-git remote add arpakit_gitlab_1 git@gitlab.com:arpakit/arpakitlib.git
--- command/git_set_divarteam_origin.sh
+++
@@ -1,4 +0,0 @@
-cd ..
-
-git remote remove divarteam_github_1
-git remote add divarteam_github_1 git@github.com:divarteam/arpakitlib.git
