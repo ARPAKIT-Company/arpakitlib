669d752b60b0354e0eb7a155b10ee1e2206820e1
arpakit <arpakit@gmail.com>
2025-11-14 12:57:20 +0500

    fix

--- toolbox/docker_run_portainer.sh
+++ toolbox/docker_run_portainer.sh
@@ -1,6 +1,8 @@
 docker pull portainer/portainer-ce:lts
+
 docker stop arpakit_portainer
 docker rm arpakit_portainer
+
 docker run -d \
   -p 50533:8000 \
   -p 50534:9443 \
@@ -9,4 +11,5 @@ docker run -d \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v arpakit_portainer_volume:/data \
   portainer/portainer-ce:latest
+
 echo "web port: 50534"
