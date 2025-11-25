c8ab2e70fd7c7487b9544e938ab5c7d5ecb9fa7c
arpakit <arpakit@gmail.com>
2025-11-25 16:31:16 +0500

    fix

--- arpakitlib/ar_ssh_runner_util.py
+++ arpakitlib/ar_ssh_runner_util.py
@@ -10,48 +10,9 @@ from typing import Any
 
 import asyncssh
 import paramiko
-from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.primitives.asymmetric import ed25519
-from pydantic import BaseModel, ConfigDict, Field
+from pydantic import BaseModel, ConfigDict
 
 from arpakitlib.ar_json_util import transfer_data_to_json_str
-
-
-class SSHKeys(BaseModel):
-    private_key: str = Field()
-    public_key: str = Field()
-
-
-def generate_ed25519_via_asyncssh_ssh_keys() -> SSHKeys:
-    key = asyncssh.generate_private_key("ssh-ed25519")
-
-    private_key_str = key.export_private_key().decode()
-    public_key_str = key.export_public_key().decode()
-
-    return SSHKeys(
-        private_key=private_key_str,
-        public_key=public_key_str
-    )
-
-
-def generate_ed25519_via_cryptography_ssh_keys() -> SSHKeys:
-    private_key = ed25519.Ed25519PrivateKey.generate()
-
-    private_bytes = private_key.private_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PrivateFormat.OpenSSH,
-        encryption_algorithm=serialization.NoEncryption()
-    )
-
-    public_key = private_key.public_key().public_bytes(
-        encoding=serialization.Encoding.OpenSSH,
-        format=serialization.PublicFormat.OpenSSH
-    )
-
-    return SSHKeys(
-        private_key=private_bytes.decode(),
-        public_key=public_key.decode()
-    )
 
 
 class BaseSSHException(Exception):
