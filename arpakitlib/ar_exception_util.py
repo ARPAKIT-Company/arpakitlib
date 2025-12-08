ceffcc21b87dd0ffab3751e3bb4f973beb188b86
arpakit <arpakit@gmail.com>
2025-12-08 14:19:02 +0500

    fix

--- arpakitlib/ar_exception_util.py
+++ arpakitlib/ar_exception_util.py
@@ -4,7 +4,10 @@ _ARPAKIT_LIB_MODULE_VERSION = "3.0"
 
 
 def exception_to_traceback_str(exception: BaseException) -> str:
-    return "".join(traceback.format_exception(type(exception), exception, exception.__traceback__))
+    try:
+        return "".join(traceback.format_exception(type(exception), exception, exception.__traceback__))
+    except Exception as exception_:
+        return f"Failed to format exception to traceback str {exception!r}: {exception_!r}"
 
 
 def __example():
