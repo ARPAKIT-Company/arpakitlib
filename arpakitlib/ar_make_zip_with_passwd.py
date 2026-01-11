b0ea7224ac3d0113607229456b127f43896c6f39
arpakit <arpakit@gmail.com>
2026-01-12 02:37:11 +0500

    fix

--- arpakitlib/ar_make_zip_with_passwd.py
+++ arpakitlib/ar_make_zip_with_passwd.py
@@ -8,7 +8,7 @@ def make_zip_with_passwd(
         input_filepath: str,
         output_filename: str = "archive.zip",
         passwd: str = "123"
-):
+) -> str:
     pyminizip.compress(
         input_filepath,  # исходный файл
         None,
@@ -16,4 +16,5 @@ def make_zip_with_passwd(
         passwd,  # пароль
         5  # уровень сжатия (1–9)
     )
+    return output_filename
 
